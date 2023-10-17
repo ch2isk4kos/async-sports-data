be2b2cbab13357b0e1ed7f40e03f36e1a90fe953
ch2isk4kos <ch2isk4kos@gmail.com>
2023-10-17 19:00:45 -0400

    test nextui component

--- next-client/app/page.tsx
+++ next-client/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@nextui-org/button";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
       <h1>Home</h1>
+      <Button>Enter</Button>
     </main>
   )
 }
