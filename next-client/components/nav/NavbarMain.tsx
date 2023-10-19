1a80c7d98ba3cacc2c0cf5cb78a0950e5c8a308f
ch2isk4kos <ch2isk4kos@gmail.com>
2023-10-19 11:52:28 -0400

    import/render dropdown menu boilercode

--- next-client/components/nav/NavbarMain.tsx
+++ next-client/components/nav/NavbarMain.tsx
@@ -1,15 +1,36 @@
 "use client"
 import { useState } from "react"
-import { Button, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from "@nextui-org/react"
+import { 
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Link,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle
+} from "@nextui-org/react"
 
 export default function NavbarMain() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const menutItems = [
+  const menuItems = [
     "About",
     "Contact",
     "Something Else"
   ]
+
+  const dropdownIcons = {
+    about: "AIcon",
+    chevron: "ChevIcon",
+    contact: "CIcon",
+    else: "EIcon"
+  }
 
   return (
     <Navbar onMenuOpenChange={setIsMenuOpen} isBordered>
@@ -20,6 +41,22 @@ export default function NavbarMain() {
           aria-label={isMenuOpen ? "Close" : "Open"}
           className="sm:hidden"
         />
+        {/* Menu */}
+        <NavbarMenu>
+          { menuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link
+                className="w-full"
+                href="#"
+                color={
+                  index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+                  }
+                size="lg">
+                  {item}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
         {/* LOGO */}
         <NavbarBrand>
           <h1>LOGO</h1>
@@ -28,17 +65,17 @@ export default function NavbarMain() {
       {/* Links */}
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
-          <Link color="foreground" href="#">
+          <Link href="#" color="foreground">
             MLB
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link href="#" aria-current="page">
+          <Link href="#" color="foreground">
             NFL
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">
+          <Link href="#" color="foreground">
             NBA
           </Link>
         </NavbarItem>
@@ -46,25 +83,57 @@ export default function NavbarMain() {
       {/* Right Side Content */}
       <NavbarContent justify="end">
         {/* Signup */}
-        <Button as={Link} href="#" color="primary" variant="flat">
+        {/* <Button as={Link} href="#" color="primary" variant="flat">
           SignUp
-        </Button>
-        {/* Menu */}
-        <NavbarMenu>
-          { menutItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                className="w-full"
-                href="#"
-                color={
-                  index === 2 ? "primary" : index === menutItems.length - 1 ? "danger" : "foreground"
-                  }
-                size="lg">
-                  {item}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </NavbarMenu>
+        </Button> */}
+        {/* Dropdown Root */}
+        <Dropdown>
+          <NavbarItem>
+            {/* Dropdown Toggle */}
+            <DropdownTrigger>
+              <Button
+                className="p-0 bg-transparent data-[hover=true]:bg-transparent"
+                disableRipple
+                endContent={dropdownIcons.chevron}
+                radius="sm"
+                variant="light"
+              >
+                v More
+              </Button>
+            </DropdownTrigger>
+          </NavbarItem>
+          {/* Dropdown Menu */}
+          <DropdownMenu
+            className="w-[340px]"
+            aria-label="More"
+            itemClasses={{ base: 'gap-4' }}
+          >
+            {/* About */}
+            <DropdownItem
+              key="about"
+              description="about description"
+              startContent={dropdownIcons.about}
+            >
+              About
+            </DropdownItem>
+            {/* Contact */}
+            <DropdownItem
+              key="contact"
+              description="contact description"
+              startContent={dropdownIcons.contact}
+            >
+              Contact
+            </DropdownItem>
+            {/* Something Else */}
+            <DropdownItem
+              key="else"
+              description="else description"
+              startContent={dropdownIcons.else}
+            >
+              Something Else
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </NavbarContent>
     </Navbar>
   )
