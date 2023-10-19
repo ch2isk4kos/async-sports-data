75a3fec029e3a9f25a32a5ec436deb2cf39c0920
ch2isk4kos <ch2isk4kos@gmail.com>
2023-10-19 11:12:37 -0400

    remove custom tailwind from main element

--- next-client/app/page.tsx
+++ next-client/app/page.tsx
@@ -1,11 +1,12 @@
 import NavbarMain from "@/components/nav/NavbarMain";
-import { main }from "../tailwind/home";
+// import { main }from "../tailwind/home";
 
 export default function Home() {
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     // <main className="flex min-h-screen flex-col p-24">
-    <main className={main}>
+    // <main className={main}>
+    <main>
       <NavbarMain />
       <h1>Home</h1>
     </main>
