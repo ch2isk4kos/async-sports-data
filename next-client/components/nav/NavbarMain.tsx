31319f97a04b2c6b86d98e0749f3b386a1cc12f0
ch2isk4kos <ch2isk4kos@gmail.com>
2023-10-19 11:01:53 -0400

    import useState hook & set menu state to false

--- next-client/components/nav/NavbarMain.tsx
+++ next-client/components/nav/NavbarMain.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react"
 import { Button, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from "@nextui-org/react"
 
 export default function NavbarMain() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+  const menutItems = [
+    "About",
+    "Contact",
+    "Something Else"
+  ]
+
   return (
-    <Navbar>
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
       {/* LOGO */}
       <NavbarBrand>
         <h1>LOGO</h1>
@@ -27,9 +36,26 @@ export default function NavbarMain() {
       </NavbarContent>
       {/* Right Side Content */}
       <NavbarContent justify="end">
+        {/* Signup Button */}
         <Button as={Link} href="#" color="primary" variant="flat">
           SignUp
         </Button>
+        {/* Navbar Menu */}
+        <NavbarMenu>
+          { menutItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link
+                className="w-full"
+                href="#"
+                color={
+                  index === 2 ? "primary" : index === menutItems.length - 1 ? "danger" : "foreground"
+                  }
+                size="lg">
+                  {item}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
       </NavbarContent>
     </Navbar>
   )
