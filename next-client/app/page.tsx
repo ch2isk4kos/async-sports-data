e98c90c9532af9a77b274f6e512eda70e014fa67
ch2isk4kos <ch2isk4kos@gmail.com>
2023-10-19 10:18:51 -0400

    import tailwind variable(s)

--- next-client/app/page.tsx
+++ next-client/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from "@nextui-org/button";
+import { main }from "../tailwind/home";
 
 export default function Home() {
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <main className="">
+    // <main className="flex min-h-screen flex-col p-24">
+    <main className={main}>
       <h1>Home</h1>
       <Button>Enter</Button>
     </main>
