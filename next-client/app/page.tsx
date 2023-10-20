a324aee6985fa404e019cc53779c711ab4617ca7
ch2isk4kos <ch2isk4kos@gmail.com>
2023-10-20 19:59:51 -0400

    add tailwind attributes for global layout

--- next-client/app/page.tsx
+++ next-client/app/page.tsx
@@ -7,7 +7,8 @@ export default function Home() {
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     // <main className="flex min-h-screen flex-col p-24">
     // <main className={main}>
-    <main className="max-w-screen h-screen border-2 border-blue-700">
+    // <main className="max-w-screen max-h-screen border-2 border-blue-700">
+    <main className="absolute w-auto h-auto p-0 md:p-10 border-2 border-blue-700">
       <NavbarMain />
       <HomeHeader />
     </main>
