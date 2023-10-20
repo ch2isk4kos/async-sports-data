5113537774c4c67a060ee7fe23ca79ffd331722a
ch2isk4kos <ch2isk4kos@gmail.com>
2023-10-20 15:39:56 -0400

    working on home page header: commented some code and changed tailwind classes; added articles array for sect 2 of header

--- next-client/components/home/HomeHeader.tsx
+++ next-client/components/home/HomeHeader.tsx
@@ -1,14 +1,18 @@
 "use client"
-import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+import { Card, CardHeader, CardBody, CardFooter, Image } from "@nextui-org/react";
 // import Image from "next/image";
 
 export default function HomeHeader() {
+
+  const articles = [
+    { img: "", alt: "", desc: "", url: ""},
+  ]
   return (
-    <section className="flex flex-col md:flex-row items-center h-auto border-2 border-rose-500">
+    <section className="flex flex-col md:flex-row h-30px border-2 border-rose-500">
       {/* Row Column 1 */}
       {/* <div className="basis-1/2 h-auto border-2 border-blue-500"> */}
-      <div className="w-[100%] h-auto border-2 border-blue-500">
-        <Image
+      <div className="w-[50%] h-auto border-2 border-blue-500">
+        {/* <Image
           // className="z-10 md:object-fit"
           className="z-10 md:object-cover"
           src={`https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg`}
@@ -18,19 +22,32 @@ export default function HomeHeader() {
           alt="Maxx Crosby's safety sack on Mac Mac Jones"
         />
         <h3>Let Davante Play!!</h3>
-        <p>blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p>
+        <p>blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p> */}
+        <Card
+          className="border-none"
+          isFooterBlurred
+          radius="lg"
+        >
+          <Image
+            className="z-10 object-fit"
+            // width={500}
+            // height={500}
+            src={`https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg`}
+            alt="Davante Adams making a play on the ball"
+          />
+          <CardFooter className="justify-between">
+            <p className="text-tiny text-white/80 border-white/80">
+              blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+            </p>
+          </CardFooter>
+        </Card>
       </div>
       {/* Row Column 2 */}
-      <div className="invisible md:visible w-[30%] items-center border-2 border-blue-500">
+      <div className="invisible md:visible w-[25%] items-center border-2 border-blue-500">
       {/* <div className="invisible md:visible basis-1/4 h-auto border-2 border-blue-500"> */}
       {/* <div className="hidden md:block w-[33%] h-auto border-2 border-blue-500"> */}
         <h1>section 2</h1>
-        <Card className="p-2 m-2">
-          {/* <CardHeader className="flex-col items-start">
-            <p>Headline</p>
-            <small>description</small>
-            <h4>Read More</h4>
-          </CardHeader> */}
+        <Card className="">
           <CardBody className="overflow-visible">
             <Image
               className="object-cover rounded-xl"
@@ -40,11 +57,6 @@ export default function HomeHeader() {
           </CardBody>
         </Card>
         <Card className="p-2 m-2">
-          {/* <CardHeader className="flex-col items-start">
-            <p>Headline</p>
-            <small>description</small>
-            <h4>Read More</h4>
-          </CardHeader> */}
           <CardBody className="overflow-visible">
             <Image
               className="object-cover rounded-xl"
@@ -54,11 +66,6 @@ export default function HomeHeader() {
           </CardBody>
         </Card>
         <Card className="p-2 m-2">
-          {/* <CardHeader className="flex-col items-start">
-            <p>Headline</p>
-            <small>description</small>
-            <h4>Read More</h4>
-          </CardHeader> */}
           <CardBody className="overflow-visible">
             <Image
               className="object-cover rounded-xl"
@@ -69,7 +76,7 @@ export default function HomeHeader() {
         </Card>
       </div>
       {/* Row Column 3 */}
-      <div className="invisible md:visible flex flex-col items-center w-[30%] h-auto gap-2 my-4 border-2 border-blue-500">
+      <div className="invisible md:visible flex flex-col items-center w-[25%] border-2 border-blue-500">
       {/* <div className="invisible md:visible basis-1/4 h-auto border-2 border-blue-500"> */}
       {/* <div className="hidden md:block w-[33%] h-auto border-2 border-blue-500"> */}
         <h1>section 3</h1>
