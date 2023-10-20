08576fb863d8540afa582e10fb43193e62d1fce7
ch2isk4kos <ch2isk4kos@gmail.com>
2023-10-20 16:40:42 -0400

    implement responsive design to elements of header columns

--- next-client/components/home/HomeHeader.tsx
+++ next-client/components/home/HomeHeader.tsx
@@ -1,28 +1,21 @@
 "use client"
-import { Card, CardHeader, CardBody, CardFooter, Image } from "@nextui-org/react";
+import { Button, Card, CardHeader, CardBody, CardFooter, Image } from "@nextui-org/react";
 // import Image from "next/image";
 
 export default function HomeHeader() {
-
-  const articles = [
-    { img: "", alt: "", desc: "", url: ""},
+  const blogs = [
+    { alt: "Davante Adams making a play on the ball", headline: "Adams Goes Off", img: "https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg"},
+    { alt: "Davante Adams making a play on the ball", headline: "Adams Goes Off", img: "https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg"},
+    { alt: "Davante Adams making a play on the ball", headline: "Adams Goes Off", img: "https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg"},
+    { alt: "Davante Adams making a play on the ball", headline: "Adams Goes Off", img: "https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg"},
   ]
+
   return (
-    <section className="flex flex-col md:flex-row h-30px border-2 border-rose-500">
+    // <section className="flex flex-col w-auto h-auto gap-2 m-4 p-4 md:flex-row border-2 border-rose-500">
+    <section className="flex flex-col w-auto h-auto gap-2 m-4 p-4 md:flex-row border-2 border-rose-500">
       {/* Row Column 1 */}
       {/* <div className="basis-1/2 h-auto border-2 border-blue-500"> */}
-      <div className="w-[50%] h-auto border-2 border-blue-500">
-        {/* <Image
-          // className="z-10 md:object-fit"
-          className="z-10 md:object-cover"
-          src={`https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg`}
-          // width={0}
-          // height={0}
-          radius="none"
-          alt="Maxx Crosby's safety sack on Mac Mac Jones"
-        />
-        <h3>Let Davante Play!!</h3>
-        <p>blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah</p> */}
+      <div className="w-[100%] md:w-[40%] border-2 border-blue-500">
         <Card
           className="border-none"
           isFooterBlurred
@@ -30,6 +23,7 @@ export default function HomeHeader() {
         >
           <Image
             className="z-10 object-fit"
+            // className="z-10 md:object-cover"
             // width={500}
             // height={500}
             src={`https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg`}
@@ -43,40 +37,38 @@ export default function HomeHeader() {
         </Card>
       </div>
       {/* Row Column 2 */}
-      <div className="invisible md:visible w-[25%] items-center border-2 border-blue-500">
+      <div className="w-[100%] md:w-[30%] p-2 border-2 border-blue-500">
+      {/* <div className="invisible md:visible w-[30%] h- p-2 border-2 border-blue-500"> */}
       {/* <div className="invisible md:visible basis-1/4 h-auto border-2 border-blue-500"> */}
       {/* <div className="hidden md:block w-[33%] h-auto border-2 border-blue-500"> */}
-        <h1>section 2</h1>
-        <Card className="">
-          <CardBody className="overflow-visible">
-            <Image
-              className="object-cover rounded-xl"
-              src={`https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg`}
-              alt="picture description"
-            />
-          </CardBody>
-        </Card>
-        <Card className="p-2 m-2">
-          <CardBody className="overflow-visible">
-            <Image
-              className="object-cover rounded-xl"
-              src={`https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg`}
-              alt="picture description"
-            />
-          </CardBody>
-        </Card>
-        <Card className="p-2 m-2">
-          <CardBody className="overflow-visible">
-            <Image
-              className="object-cover rounded-xl"
-              src={`https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg`}
-              alt="picture description"
-            />
-          </CardBody>
-        </Card>
+        <div className="grid grid-cols-2 gap-4 border-2 border-yellow-500">
+          {
+            blogs && blogs.map((blog) => (
+              <Card
+                className="border-none"
+                radius="lg"
+                isFooterBlurred
+              >
+                <Image
+                  // className="object-cover"
+                  className="object-fit"
+                  src={blog.img}
+                  alt={blog.alt}
+                />
+                <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+                  <p className="text-tiny text-white/80">{blog.headline}</p>
+                  <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg" size="sm">
+                    Read More
+                  </Button>
+                </CardFooter>
+              </Card>
+            ))
+          }
+        </div>
       </div>
       {/* Row Column 3 */}
-      <div className="invisible md:visible flex flex-col items-center w-[25%] border-2 border-blue-500">
+      <div className="w-[100%] md:w-[30%] p-2 border-2 border-blue-500">
+      {/* <div className="invisible md:visible flex flex-col items-center w-[30%] border-2 border-blue-500"> */}
       {/* <div className="invisible md:visible basis-1/4 h-auto border-2 border-blue-500"> */}
       {/* <div className="hidden md:block w-[33%] h-auto border-2 border-blue-500"> */}
         <h1>section 3</h1>
