cdd93d7263a11a577dec3a4634942a4d421b678b
ch2isk4kos <ch2isk4kos@gmail.com>
2023-10-20 18:35:56 -0400

    adjust column 2 placement within its parent

--- next-client/components/home/HomeHeader.tsx
+++ next-client/components/home/HomeHeader.tsx
@@ -1,13 +1,24 @@
 "use client"
-import { Button, Card, CardHeader, CardBody, CardFooter, Image } from "@nextui-org/react";
+import { Button, Card, CardHeader, CardBody, CardFooter, Image, Listbox, ListboxItem } from "@nextui-org/react";
 // import Image from "next/image";
 
 export default function HomeHeader() {
   const blogs = [
-    { alt: "Davante Adams making a play on the ball", headline: "Adams Goes Off", img: "https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg"},
-    { alt: "Davante Adams making a play on the ball", headline: "Adams Goes Off", img: "https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg"},
-    { alt: "Davante Adams making a play on the ball", headline: "Adams Goes Off", img: "https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg"},
-    { alt: "Davante Adams making a play on the ball", headline: "Adams Goes Off", img: "https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg"},
+    { alt: "Davante Adams making a play on the ball", headline: "Adams Goes Off", img: "https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg", url: ""},
+    { alt: "Davante Adams making a play on the ball", headline: "Adams Goes Off", img: "https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg", url: ""},
+    { alt: "Davante Adams making a play on the ball", headline: "Adams Goes Off", img: "https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg", url: ""},
+    { alt: "Davante Adams making a play on the ball", headline: "Adams Goes Off", img: "https://s.hdnux.com/photos/01/34/56/75/24309591/3/1200x0.jpg", url: ""},
+  ]
+
+  const listItems = [
+    { key: "article-1", headline: "Astros Stay Hot!", label: "Article 1", url: ""},
+    { key: "article-2", headline: "2023-2024 NBA Season Preview", label: "Article 2", url: ""},
+    { key: "article-3", headline: "Raiders on a Winning Streak", label: "Article 3", url: ""},
+    { key: "article-4", headline: "Yankees Off Season Begins", label: "Article 4", url: ""},
+    { key: "article-5", headline: "How Will Luka and Kyrie Co-exist?", label: "Article 5", url: ""},
+    { key: "article-6", headline: "Miami is the Greatest Show on Surf", label: "Article 6", url: ""},
+    { key: "article-7", headline: "Miami is the Greatest Show on Surf", label: "Article 7", url: ""},
+    { key: "article-8", headline: "Miami is the Greatest Show on Surf", label: "Article 8", url: ""},
   ]
 
   return (
@@ -37,11 +48,11 @@ export default function HomeHeader() {
         </Card>
       </div>
       {/* Row Column 2 */}
-      <div className="w-[100%] md:w-[40%] py-2 px-2 border-2 border-blue-500">
+      <div className="w-[100%] md:w-[40%] border-2 border-blue-500">
       {/* <div className="invisible md:visible w-[30%] h- p-2 border-2 border-blue-500"> */}
       {/* <div className="invisible md:visible basis-1/4 h-auto border-2 border-blue-500"> */}
       {/* <div className="hidden md:block w-[33%] h-auto border-2 border-blue-500"> */}
-        <div className="grid grid-cols-2 gap-4 content-center border-2 border-yellow-500">
+        <div className="grid grid-cols-2 gap-2 h-full border-2 border-yellow-500">
           {
             blogs && blogs.map((blog) => (
               <Card
@@ -67,11 +78,36 @@ export default function HomeHeader() {
         </div>
       </div>
       {/* Row Column 3 */}
-      <div className="w-[100%] md:w-[30%] p-2 border-2 border-blue-500">
+      <div className="w-[100%] md:w-[20%] p-2 border-2 border-blue-500">
       {/* <div className="invisible md:visible flex flex-col items-center w-[30%] border-2 border-blue-500"> */}
       {/* <div className="invisible md:visible basis-1/4 h-auto border-2 border-blue-500"> */}
       {/* <div className="hidden md:block w-[33%] h-auto border-2 border-blue-500"> */}
-        <h1>section 3</h1>
+        <div className="">
+          { 
+          listItems && listItems.map((item) => (
+            <Listbox
+              aria-label={item.label}
+              onAction={(key) => alert(key)}
+              className="p-0 gap-0 divide-y divide-default-300/50 dark:divide-default-100/80 bg-content1 max-w-[300px] overflow-visible shadow-small rounded-medium"
+              itemClasses={{
+                base: "px-3 first:rounded-t-medium last:rounded-b-medium rounded-none gap-3 h-12 data-[hover=true]:bg-default-100/80",
+              }}
+            >
+              <ListboxItem
+                key={item.key}
+                // endContent={<ItemCounter number={13} />}
+                // startContent={
+                //   <IconWrapper className="bg-success/10 text-success">
+                //     <BugIcon className="text-lg " />
+                //   </IconWrapper>
+                // }
+              >
+                {item.headline}
+              </ListboxItem>
+            </Listbox>
+          ))
+        }
+        </div>
       </div>
     </section>
   )
