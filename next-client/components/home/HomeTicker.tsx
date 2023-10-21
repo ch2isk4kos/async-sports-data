b9dd19d8d8983cb4629c2d8fd6dabf5634f38cfd
ch2isk4kos <ch2isk4kos@gmail.com>
2023-10-21 14:58:37 -0400

    add props to pause/resume animation on hover

--- next-client/components/home/HomeTicker.tsx
+++ next-client/components/home/HomeTicker.tsx
@@ -1,29 +1,32 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 import Ticker from 'framer-motion-ticker';
 
 export default function HomeTicker() {
+  const [isPlaying, setIsPlaying] = useState(true);
+
   const scores = [
-    { teamName1: "Las Vegas Raiders", teamName2: "Kansas City Chiefs", team1Score: "17", team2Score: "24"},
-    { teamName1: "Las Vegas Raiders", teamName2: "Kansas City Chiefs", team1Score: "17", team2Score: "24"},
-    { teamName1: "Las Vegas Raiders", teamName2: "Kansas City Chiefs", team1Score: "17", team2Score: "24"},
-    { teamName1: "Las Vegas Raiders", teamName2: "Kansas City Chiefs", team1Score: "17", team2Score: "24"},
-    { teamName1: "Las Vegas Raiders", teamName2: "Kansas City Chiefs", team1Score: "17", team2Score: "24"},
-    { teamName1: "Las Vegas Raiders", teamName2: "Kansas City Chiefs", team1Score: "17", team2Score: "24"},
-    { teamName1: "Las Vegas Raiders", teamName2: "Kansas City Chiefs", team1Score: "17", team2Score: "24"},
+    { teamName1: "LV", teamName2: "KC", team1Score: "17", team2Score: "24"},
+    { teamName1: "LV", teamName2: "KC", team1Score: "17", team2Score: "24"},
+    { teamName1: "LV", teamName2: "KC", team1Score: "17", team2Score: "24"},
+    { teamName1: "LV", teamName2: "KC", team1Score: "17", team2Score: "24"},
+    { teamName1: "LV", teamName2: "KC", team1Score: "17", team2Score: "24"},
+    { teamName1: "LV", teamName2: "KC", team1Score: "17", team2Score: "24"},
+    { teamName1: "LV", teamName2: "KC", team1Score: "17", team2Score: "24"},
   ];
 
   return (
-    <div className="">
-      <Ticker duration={20}>
+    <div className="absolute w-full invisible md:visible border-2 border-rose-500">
+      <Ticker duration={20} onMouseEnter={() => setIsPlaying(false)} onMouseLeave={() => setIsPlaying(true)} isPlaying={isPlaying}>
         {scores.map((score, index) => (
           <div
             key={index}
-            // style={{
-            //   backgroundColor: item,
-            //   margin: '5px',
-            //   height: '250px',
-            //   width: '200px',
-            // }}
+            style={{
+              // backgroundColor: item,
+              margin: '5px',
+              height: '5%',
+              width: '200px',
+            }}
           >
             <p>{score.teamName1}</p> | <p>{score.teamName2}</p>
           </div>
