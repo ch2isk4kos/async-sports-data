e1dbb8431ee40c5ab6eb9c4015b8985d8d141087
ch2isk4kos <ch2isk4kos@gmail.com>
2023-10-21 10:55:51 -0400

    move/import/render navbar

--- next-client/app/layout.tsx
+++ next-client/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Providers } from "./providers";
+import NavbarMain from "@/components/nav/NavbarMain";
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body className={inter.className}>
         <Providers>
+          <NavbarMain />
           {children}
         </Providers>
       </body>
