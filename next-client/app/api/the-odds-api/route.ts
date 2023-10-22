8c7298518074703a9f8658f0c7a586ff2f35504c
ch2isk4kos <ch2isk4kos@gmail.com>
2023-10-22 12:33:06 -0400

    create route handler to fetch data for home page ticker

--- next-client/app/api/the-odds-api/route.ts
+++
@@ -1,6 +0,0 @@
-export async function GET() {
-  const apiKey = `${process.env.NEXT_PUBLIC_THE_ODDS_API_KEY}`;
-  const res = await fetch(`https://api.the-odds-api.com/v4/sports/americanfootball_nfl/scores?apiKey=${apiKey}`);
-  const data = await res.json();
-  return Response.json({ data });
-}
