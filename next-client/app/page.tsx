88503f3a4406e0b12ab8ca5b378bbf660975e748
ch2isk4kos <ch2isk4kos@gmail.com>
2023-10-28 22:50:22 -0400

    remove comment(s)

--- next-client/app/page.tsx
+++ next-client/app/page.tsx
@@ -5,7 +5,6 @@ import HomeTicker from "@/components/home/HomeTicker";
 export default function Home() {
   return (
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    // <main className="flex min-h-screen flex-col p-24">
     // <main className={main}>
     // <main className="max-w-screen max-h-screen border-2 border-blue-700">
     // <main className="absolute w-auto h-auto p-0 md:px-20 lg:px-40 xl:px-60 border-2 border-blue-700">
